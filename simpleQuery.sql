a34306b0eb447d28693a0bdc909f0e938c888fbc
Ertugrul Eren Selcuk <83607933+ertugrulerenselcuk@users.noreply.github.com>
2024-12-06 21:41:12 +0300

    simpleQuery.sql

--- simpleQuery.sql
+++ simpleQuery.sql
@@ -1,3 +1,8 @@
+/* first name and last name with e-mal
+   salaries with a 35% increase
+*/
+
 select first_name, last_name
   first_name || '.' || last_name ||'tutorial.com'
+  salary * 1.35
 from employees;
